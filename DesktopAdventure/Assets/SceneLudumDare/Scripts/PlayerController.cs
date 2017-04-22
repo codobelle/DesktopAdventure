5d55cbccdf3a9ae89ed3712246a2eb95bc766371
elena <casapciuce@gmail.com>
2017-04-22 19:27:46 +0300

    FirstPush

---
+++ DesktopAdventure/Assets/SceneLudumDare/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    [HideInInspector]
+    public bool facingRight = true;
+    [HideInInspector]
+    public bool jump = false;
+    public float maxSpeed = 5f;
+    public float jumpForce = 1000f;
+    public Transform groundCheck;
+
+
+    private bool grounded = false;
+    //private Animator anim;
+    private Rigidbody2D rb2d;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        // anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    //void OnCollisionEnter2D(Collision2D coll)
+    //{
+    //    Debug.Log(coll.collider.gameObject.layer);
+    //    if (coll.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+    //    {
+    //        grounded = true;
+
+    //        print("Collision");
+    //    }
+    //}
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        print(grounded);
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        rb2d.velocity = new Vector2(h * maxSpeed * Time.deltaTime, rb2d.velocity.y);
+        if (h > 0 && !facingRight)
+            Flip();
+        else if (h < 0 && facingRight)
+            Flip();
+
+        if (jump)
+        {
+            jump = false;
+            grounded = false;
+            //anim.SetTrigger("Jump");
+            rb2d.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+}
+
---
+++ DesktopAdventure/Assets/SceneLudumDare/Scripts/Teleportation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+    public Transform destination;
+    public GameObject player;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject == player)
+            Teleport();
+    }
+
+    void Teleport()
+    {
+        player.transform.position = destination.position;
+    }
+}
